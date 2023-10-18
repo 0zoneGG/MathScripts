04d8905cf61e0059d172b64d1907082146c7bd41
0zoneGG <jcrojas408@gmail.com>
2023-10-18 10:27:34 -0700

    Commiting Area of rectangle

---
+++ Area_of_rectangle.js
@@ -0,0 +1,10 @@
+function rectangleArea(length, width) {
+    let area = length * width;
+    return area;
+  }
+  
+  let length = 10; 
+  let width = 5;  
+  let area = rectangleArea(length, width);
+  console.log("The area of the rectangle is: " + area);
+  
